52d4a6032da0e669df4aa8856ba3bcda9ad26843
Ryan Ramdehol <rramdeholshift@gmail.com>
2019-09-24 21:12:38 -0400

    authenticate route

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,72 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+const User = require("../models/User");
+
+// @route POST api/auth
+// @desc Auth user get token
+// @access Public
+router.post(
+  "/",
+  [
+    check("email", "Please input a valid email").isEmail(),
+    check("password", "Password is required").exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array()
+      });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email: email });
+
+      if (!user) {
+        return res.status(400).json({
+          msg: "Invalid Credentials"
+        });
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({
+          msg: "Invalid Credentials"
+        });
+      }
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("server error");
+    }
+  }
+);
 
 // @route GET api/auth
 // @desc Get logged in user
 // @access Private
 router.get("/", (req, res) => {
-  res.send("Get logged in user");
-});
-
-// @route POST api/auth
-// @desc Auth user get token
-// @access Public
-router.post("/", (req, res) => {
   res.send("Login a user");
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.get("/", (req, res) =>
 //Connect Database
 connectDB();
 
+// Init Mddleware
+app.use(express.json({ extended: false }));
+
 // Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
