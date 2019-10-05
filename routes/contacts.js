95e219b05364d68d08f5c3330e11a1bcda7d4c0a
Ryan Ramdehol <rramdeholshift@gmail.com>
2019-10-05 15:18:19 -0400

    all routes fixed

--- routes/contacts.js
+++ routes/contacts.js
@@ -110,7 +110,7 @@ router.delete("/:id", auth, async (req, res) => {
       return res.status(401).json({ msg: "Not authorized" });
     }
 
-    await Contact.findByIdAndRemove();
+    await Contact.findByIdAndRemove(req.params.id);
 
     res.json({
       msg: "Contact Removed"
